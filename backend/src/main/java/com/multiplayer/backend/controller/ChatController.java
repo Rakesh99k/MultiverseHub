6a3283d43766d005ea87c3a443a6576de9c0ad7a
Rakesh <124860454+Rakesh99k@users.noreply.github.com>
2025-09-12 23:56:54 +0530

    added backend features

---
+++ backend/src/main/java/com/multiplayer/backend/controller/ChatController.java
@@ -0,0 +1,15 @@
+package com.multiplayer.backend.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public String sayHello(String message) {
+        return "Server says: " + message;
+    }
+}
