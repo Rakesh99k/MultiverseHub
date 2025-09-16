1e1820e81a9f1d89f2b057873224f296c6900c4f
Rakesh <124860454+Rakesh99k@users.noreply.github.com>
2025-09-16 18:50:50 +0530

    added GameMoveRequest

--- backend/src/main/java/com/multiplayer/backend/dto/GameMoveRequest.java
+++ backend/src/main/java/com/multiplayer/backend/dto/GameMoveRequest.java
@@ -1,4 +1,29 @@
 package com.multiplayer.backend.dto;
 
 public class GameMoveRequest {
+    private String playerId;
+    private String move; // could be a position, action, etc.
+
+    public GameMoveRequest() {}
+
+    public GameMoveRequest(String playerId, String move) {
+        this.playerId = playerId;
+        this.move = move;
+    }
+
+    public String getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(String playerId) {
+        this.playerId = playerId;
+    }
+
+    public String getMove() {
+        return move;
+    }
+
+    public void setMove(String move) {
+        this.move = move;
+    }
 }
